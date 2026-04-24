62c995a70a976e0617962377e2ffb9cd8dfcad98
James Robinson <js.robinson@live.co.uk>
2026-04-24 19:47:54 +0100

    adding google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, Titillium_Web } from "next/font/google";
 import { SanityLive } from "@/sanity/lib/live";
+import Script from "next/script";
 import "./globals.css";
 
 const bebasNeue = Bebas_Neue({
@@ -31,6 +32,18 @@ export default function RootLayout({
       className={`${bebasNeue.variable} ${titilliumWeb.variable}`}
     >
       <body className="min-h-screen flex flex-col">{children}<SanityLive /></body>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3BLD04LXCD"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3BLD04LXCD');
+        `}
+      </Script>
     </html>
   );
 }
